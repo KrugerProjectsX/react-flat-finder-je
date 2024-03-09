9daf7dfa99d28fafe35664f168506acccfd1598c
JOSE LUIS RODRIGUEZ CANO <jrodriguez@match-working.com>
2024-03-09 13:34:15 -0500

    LogIn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
+import LogIn from './pages/LogIn';
 
 function App() {
   return (
     <Routes>
-      <Route path ={"/"} element ={<Home/>} />
+      <Route path ={"/"} element ={<LogIn/>} />
+      <Route path ={"/dashboard"} element ={<Home/>} />
     </Routes>
   
   );
