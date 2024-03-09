2a06cfdf519e36a1ec2c6654b59ef784bcd85763
JOSE LUIS RODRIGUEZ CANO <jrodriguez@match-working.com>
2024-03-09 10:59:41 -0500

    firebase

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBos2TR9XiHJHNvIxmBnbLU0JCekTAGed8",
+  authDomain: "flat-finder-45cb0.firebaseapp.com",
+  projectId: "flat-finder-45cb0",
+  storageBucket: "flat-finder-45cb0.appspot.com",
+  messagingSenderId: "206119590471",
+  appId: "1:206119590471:web:8d2b71bfe60413750a2f62"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
