466b1afdd37f6ec62b4f728733a8bb82f85021eb
JOSE LUIS RODRIGUEZ CANO <jrodriguez@match-working.com>
2024-03-09 10:33:51 -0500

    rutas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  important: '#root',
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }
 
