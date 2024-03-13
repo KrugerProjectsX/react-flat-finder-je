f129c0e2217a9be0e388650f7bb18eb851123c48
Edu_Davila <edavila@krugerschool.edu.ec>
2024-03-13 18:19:02 -0500

    Components to components

---
+++ src/pages/NewFlat.js
@@ -0,0 +1,15 @@
+import { FlatForm } from "../components/FlatForm"
+import ResponsiveAppBar from "../components/Nabvar"
+
+
+const NewFlat = () =>{
+    
+
+    return(
+    <>
+        <ResponsiveAppBar/>
+        <FlatForm/>
+    </>)
+}
+
+export {NewFlat}
